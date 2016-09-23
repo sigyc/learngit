1403b528fdf236afd80121206203e06462e13b50
yu cheng <yucheng11235@163.com>
2016-09-23 12:43:16 +0800

    update ref

--- ref.py
+++ ref.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# 输入中英文混合文件名ex.txt作为第二参数，输出
+# 输入中英文混合文件名ex.txt(asicc编码)作为第二参数，输出
 #ex+.txt纯英文文件
 from sys import argv
 
@@ -9,7 +9,7 @@ fs = open(filename,'r')
 fd = open(filename.split('.')[0] + '+' +'.'+ 'txt','w')
 
 for x in fs.read():
-    if (ord(x)<=127):
+    if(ord(x)<=127):
         fd.write(x)
 
 fs.close()
