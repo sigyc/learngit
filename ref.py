4d84db49ef9a8b5ca28634fa996917de09fc638e
yu cheng <yucheng11235@163.com>
2016-09-23 19:24:20 +0800

    update ref

--- ref.py
+++ ref.py
@@ -1,16 +1,23 @@
 # -*- coding:utf-8 -*-
 # 输入中英文混合文件名ex.txt(asicc编码)作为第二参数，输出
 #ex+.txt纯英文文件
+# savec为保留中文行的标志
 from sys import argv
 
 script,filename = argv
+savec = '+'
 
 fs = open(filename,'r')
 fd = open(filename.split('.')[0] + '+' +'.'+ 'txt','w')
 
-for x in fs.read():
-    if(ord(x)<=127):
-        fd.write(x)
+for lines in fs.readlines(): 
+    lines = lines.replace(' ','')
+    if savec in lines:
+        fd.write(lines.replace('+',''))
+        continue
+    for x in lines:
+        if ord(x)<=127:
+            fd.write(x)
 
 fs.close()
 fd.close()
